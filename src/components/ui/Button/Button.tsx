4ed9d78ed21f4404a4ef23a9a25a0a0a0f13454a
gamerus12125 <Mik78456@yandex.ru>
2023-11-26 15:42:33 +0300

    change button design

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -11,9 +11,9 @@ const Button = ({
     <button
       type={type}
       onClick={() => funcClick? funcClick(): undefined}
-      className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
+      className="relative inline-flex items-center justify-center p-3 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg bg-purple-600"
     >
-      <span className="relative transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0 sm:px-5 py-2.5">
+      <span className="">
         {children}
       </span>
     </button>
